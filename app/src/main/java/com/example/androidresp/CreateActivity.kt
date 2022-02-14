24751f18a233b80fc64434244b63787b9c51232f
ollerexpositomartin <ollerexpositomartin@gmail.com>
2022-02-14 21:08:05 +0100

    Añado barra de navegacion

--- app/src/main/java/com/example/androidresp/CreateActivity.kt
+++ app/src/main/java/com/example/androidresp/CreateActivity.kt
@@ -2,10 +2,14 @@ package com.example.androidresp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class CreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
-    }
+
+
+        }
+
 }
--- app/src/main/java/com/example/androidresp/MainActivity.kt
+++ app/src/main/java/com/example/androidresp/MainActivity.kt
@@ -2,13 +2,38 @@ package com.example.androidresp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.androidresp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setListener()
+    }
+
+    private fun setListener() {
+        binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
+
+            when (item.itemId) {
+                R.id.boton1 -> {
+                    Log.d("BOTON1------->", "PULSADO")
+                    true
+                }
+                R.id.boton2 -> {
+                    Log.d("BOTON2------->", "PULSADO")
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+
+        }
     }
 }

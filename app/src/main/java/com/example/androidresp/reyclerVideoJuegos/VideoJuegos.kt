315f02ab42c0de7082ed98eedc5c0f7bbfac8829
Jorgegga <65369692+Jorgegga@users.noreply.github.com>
2022-02-15 19:53:41 +0100

    modificar data

--- app/src/main/java/com/example/androidresp/reyclerVideoJuegos/VideoJuegos.kt
+++ app/src/main/java/com/example/androidresp/reyclerVideoJuegos/VideoJuegos.kt
@@ -1,7 +1,10 @@
 package com.example.androidresp.reyclerVideoJuegos
 
 data class VideoJuegos(
+    val id:Int,
     val nombre:String,
     val descripcion:String,
+    val categoria:String,
+    val fecha:Int,
     val image:Int
 )
